41678e15310f094419603bd914be68bdb47de4f7
Yeongseo Kim <1903020@ewhain.net>
2023-05-22 17:10:24 +0900

    feat(View): 보이는 부분 구현

---
+++ iconControl.js
@@ -0,0 +1,25 @@
+const downIconClass = "fi fi-rr-angle-small-down";
+const rightIconClass = "fi fi-rr-angle-small-right"
+
+document.addEventListener('DOMContentLoaded', function () {
+    const excelButton = document.getElementById('excelButton');
+    excelButton.addEventListener('click', function () {
+        const CLASSTYPE = "excel";
+        ToggleIcon(excelButton);
+        ToggleBlock(CLASSTYPE);
+    });
+});
+
+function ToggleBlock(classtype) {
+    const targetBlock = document.getElementById(`${classtype}Behind`);
+    const currentDisplay = targetBlock.style.display;
+    const newDisplay = (currentDisplay === 'none') ? 'block' : 'none';
+    targetBlock.style.display = newDisplay;
+}
+
+function ToggleIcon(buttonElement) {
+    const targetIcon = buttonElement.querySelector('i');
+    const currentClass = targetIcon.className;
+    const newClass = (currentClass === rightIconClass) ? downIconClass : rightIconClass;
+    targetIcon.className = newClass;
+}
