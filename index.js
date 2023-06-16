66951256d178469af65bd246656e3673ccae25cb
Yeongseo Kim <1903020@ewhain.net>
2023-06-16 23:15:14 +0900

    feat(excel): pk, fk 버튼 토글 시 attributeState에도 상태 반영

--- index.js
+++ index.js
@@ -61,7 +61,6 @@ function makeAttributeObject() {
 
 function identifyDataType(data) {
     if (typeof data === 'number' && !isNaN(data)) return "INT";
-    if (data === "") return "NULL";
     return "CHAR"
 }
 
@@ -218,7 +217,7 @@ function editAttributeState(preName, newName) {
 }
 
 function createSheetContainerAttributeList(sheetName) {
-    const targetTable = excelState[sheetName];
+    const targetTable = attributeState[sheetName];
     const attributeBox = document.createElement("div");
 
     if (!targetTable) {
@@ -226,39 +225,39 @@ function createSheetContainerAttributeList(sheetName) {
         return attributeBox;
     }
 
-
     attributeBox.classList.add("attributeBox");
     attributeBox.id = `${sheetName}Behind`;
     attributeBox.style.display = "none";
 
-    const keys = Object.keys(targetTable[0]);
-    for (const key of keys) {
-        if (key.includes("EMPTY")) continue;
+    const attributes = Object.keys(targetTable);
+    for (const attribute of attributes) {
+        const targetObject = targetTable[attribute];
         const attributeList = document.createElement("tr"); //행 생성
 
         const tdName = document.createElement("td"); //이름 열에는 키
-        tdName.innerText = key;
+        tdName.innerText = attribute;
         attributeList.appendChild(tdName);
 
         const tdDataType = document.createElement("td"); //데이터타입
         tdDataType.contentEditable = true;
-        //데이터 타입 갖고 오는 함수
-        tdDataType.innerText = DATATYPE[0];
+        tdDataType.innerText = targetObject.dataType;
         tdDataType.addEventListener("keypress", editDataType);
-        tdDataType.addEventListener("blur", editDataType);
-        identifyDataType(sheetName, key);
 
         const constraintsPrimaryKey = document.createElement("td");
         const isPrimaryKey = document.createElement("button");
+        isPrimaryKey.id = `${sheetName}-${attribute}-pk`;
         isPrimaryKey.innerText = "PK";
-        isPrimaryKey.classList.add("constraintsButton");
+        const pkClassName = determineKeyClassName(targetObject.pk);
+        isPrimaryKey.classList.add(pkClassName);
         isPrimaryKey.addEventListener("click", constraintsHandler);
         constraintsPrimaryKey.appendChild(isPrimaryKey);
 
         const constraintsForeignKey = document.createElement("td");
         const isForeignKey = document.createElement("button");
+        isForeignKey.id = `${sheetName}-${attribute}-fk`;
         isForeignKey.innerText = "FK";
-        isForeignKey.classList.add("constraintsButton");
+        const fkClassName = determineKeyClassName(targetObject.fk);
+        isForeignKey.classList.add(fkClassName)
         isForeignKey.addEventListener("click", constraintsHandler)
         constraintsForeignKey.appendChild(isForeignKey);
 
@@ -273,8 +272,12 @@ function createSheetContainerAttributeList(sheetName) {
     return attributeBox
 }
 
+function determineKeyClassName(targetBool) {
+    return targetBool ? "constraintsButton selectedButton" : "constraintsButton";
+}
+
 function editDataType(e) {
-    if (window.event.keyCode == 13 || e.type == "blur") {
+    if (window.event.keyCode == 13) {
         e.preventDefault();
         document.activeElement.blur();
         const editedDataType = e.target.innerText;
@@ -285,11 +288,15 @@ function editDataType(e) {
 
 function constraintsHandler(e) {
     const COLOR_CLASS_NAME = "selectedButton";
-    console.dir(e.target.classList);
-    if (e.target.classList.contains(COLOR_CLASS_NAME)) {
-        e.target.classList.remove(COLOR_CLASS_NAME)
+    const targetClassList = e.target.classList;
+    const [tableName, attributeName, targetKey] = e.target.id.split("-");
+    if (targetClassList.contains(COLOR_CLASS_NAME)) {
+        targetClassList.remove(COLOR_CLASS_NAME);
+        attributeState[tableName][attributeName][targetKey] = false;
     }
     else {
-        e.target.classList.add(COLOR_CLASS_NAME);
+        targetClassList.add(COLOR_CLASS_NAME);
+        attributeState[tableName][attributeName][targetKey] = true;
     }
+    console.log(attributeState);
 }
